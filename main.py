a8f1cf1fb47ca73cd1120e2097209d61af07763d
HectorBerger <1751751@uab.cat>
2025-04-22 11:52:47 +0200

    Clases + csv.import

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 #Projecte
+
+import csv
+import numpy as np
+
+#Leer bases de datos
+NOMFITXER = "datasets/books/Books.csv"
+with open(NOMFITXER) as csvfile:
+    bookreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in bookreader:
+        print(', '.join(row))
+
+class Recomenador:
+    def __init__(self):
+        self._items = []
+
+
+class User:
+    def __init__(self):
+        self._id = str()
+
+from abc import ABC, abstractmethod 
+class Item(ABC):
+    def __init__(self):
+        self._id = str()
+
+
+
+class Movie(Item):
+    def __init__(self):
+        self._id = str()
+        
+class Book(Item):
+    def __init__(self):
+        self._id = str()
