3cb6b24076853b8e5e3cb84390d8edad44c5a222
HectorBerger <1751751@uab.cat>
2025-04-23 10:42:48 +0200

    limpiar main

--- main.py
+++ main.py
@@ -4,26 +4,3 @@ import csv
 import numpy as np
 
 while True:
-
-class Recomenador:
-    def __init__(self):
-        self._items = []
-     
-
-class User:
-    def __init__(self):
-        self._id = str()
-
-from abc import ABC, abstractmethod 
-class Item(ABC):
-    def __init__(self):
-        self._id = str()
-
-
-class Movie(Item):
-    def __init__(self):
-        self._id = str()
-
-class Book(Item):
-    def __init__(self):
-        self._id = str()
