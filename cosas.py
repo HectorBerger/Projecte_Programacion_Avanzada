17868e8259dc69b07da8058e1040ff4d1518b08c
HectorBerger <1751751@uab.cat>
2025-04-29 22:00:32 +0200

    pequeños cambios

---
+++ cosas.py
@@ -0,0 +1,26 @@
+import logging
+
+
+def func(a, b):
+    """
+    Short summary of the function.
+
+    Extended description of the function.
+
+    Parameters
+    ----------
+    a : int
+        Description of parameter `a`.
+    b : str
+        Description of parameter `b`.
+
+    Returns
+    -------
+    int
+        Description of the return value.
+
+    Raises
+    ------
+    ValueError
+        If `a` is less than zero.
+    """
