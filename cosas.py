7fa88e38009fdd300803be45dc28d0e96ce6f881
HectorBerger <1751751@uab.cat>
2025-05-11 01:26:33 +0200

    Argparser+Pickle inacabado

--- cosas.py
+++ cosas.py
@@ -4,6 +4,28 @@ import logging
 logging.basicConfig(filename='log.txt',level=logging.INFO,
 format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 
+from tqdm import tqdm
+import threading
+import time
+
+def executar_amb_barra(func, *args, **kwargs):
+    resultat = [None]
+
+    def target():
+        resultat[0] = func(*args, **kwargs)
+
+    fil = threading.Thread(target=target)
+    fil.start()
+
+    with tqdm(desc=f"Executant '{func.__name__}'", bar_format='{l_bar}{bar}| {elapsed} s') as barra:
+        while fil.is_alive():
+            time.sleep(0.1)
+            barra.update(1)
+
+    fil.join()
+    return resultat[0]
+
+
 def func(a, b):
     """
     Short summary of the function.
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 #Projecte
 import pickle
+import argparse
 import logging
 from Recomenador import Recomenador
 from dataset import DatasetMovies, DatasetBooks
 
 
 
-#dataset = arg1
-#method = arg2
-
-#main prueba
 #DatasetBooks()DatasetMovies
+def main():
 
-if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Aplicar un algoritmo de recomendación a un dataset.") #Hemos usado argparse para poder mostrar el help más fácilmente
+    parser.add_argument("dataset", choices=["","",""], help="") #default?  type=str?=>not necessary
+    parser.add_argument("method", choices=[""], help="")
 
+    args = parser.parse_args()
+    dataset = args.dataset
+    method = args.method
     recom = Recomenador(DatasetMovies())
     usuari = "23"
     recomanacio = recom.recomanacio_colaboratiu(usuari, 3)
@@ -24,8 +27,23 @@ if __name__ == "__main__":
         print(f"No hi ha recomanacions disponibles per a l'usuari {usuari}.")
 
 
+if __name__ == '__main__':
+    main()
+
+
+
 
 """
+r = Recommender()
+...
+# Per guardar en un fitxer binari una còpia exacta de l’objecte
+with open(“recommender.dat", 'wb') as fitxer:
+    pickle.dump(r, fitxer)
+
+# Per recuperar del fitxer la còpia de l’objecte
+with open(“recommender.dat", 'rb') as fitxer:
+    r = pickle.load(fitxer)
+
 if pickle:
     pickle classes carregar dades
 else:
